d49b2218f5947904b4d00f675b87eeab9b405f85
Ashu-kd <a_dubey@ee.iitr.ac.in>
2023-07-25 07:58:34 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    BitKit.setMotormoduleAction(DirectionTpye.Forward, SpeedTpye.Medium)
+    basic.showNumber(0)
+})
+input.onButtonPressed(Button.AB, function () {
+    BitKit.setMotormoduleSpeed(119, 0)
+})
+input.onButtonPressed(Button.B, function () {
+    BitKit.setMotormoduleAction(DirectionTpye.Backward, SpeedTpye.Fast)
+    basic.showNumber(1)
+})
+BitKit.onColor(ColorEvent.Other, function () {
+	
+})
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    basic.showNumber(BitKit.getColor())
+})
